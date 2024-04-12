cd799da417d7d6b7da7f7ec87e9283fa905b9b2a
Tanveer <101932506+amdtaher@users.noreply.github.com>
2024-04-12 22:47:27 +0600

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        'primary': 'hsl(26, 100%, 55%)',
-        'secondary': 'hsl(25, 100%, 94%)',
-        'dark-blue': 'hsl(220, 13%, 13%)',
-        'dark-grey-blue': 'hsl(219, 9%, 45%)',
-        'grey-blue': 'hsl(220, 14%, 75%)',
-        'light-grey-blue': 'hsl(223, 64%, 98%)',
-        'white': 'hsl(0, 0%, 100%)',
-        'black': 'hsl(0, 0%, 0%)',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
 
